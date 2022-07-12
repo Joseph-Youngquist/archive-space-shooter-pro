814903b0d8a718a0c988babcb64fcd13dc8219e7
Joe Youngquist <joseph.youngquist@gmail.com>
2022-07-12 17:11:00 -0400

    sets vertial bound limits for player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -23,5 +23,21 @@ public class Player : MonoBehaviour
         Vector3 movementVector = new Vector3(horizontalInput, verticalInput, 0);
 
         transform.Translate(movementVector * _playerMovementSpeed * Time.deltaTime);
+
+        // prevent the player from moving higher than 0 on the y position
+        // prevent the player from moving below than -3.75 on the y position
+
+        // wrap the player on the left and right bounds when player position is
+        // less than -9.2 or greater than 9.2
+
+        if (transform.position.y >= 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.y <= -3.75f)
+        {
+            transform.position = new Vector3(transform.position.x, -3.75f, 0);
+        }
+
     }
 }
