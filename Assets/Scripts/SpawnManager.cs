21f2d741f009f7e15912b454e96ad90424fa8c1a
Joe Youngquist <joseph.youngquist@gmail.com>
2022-07-14 14:54:00 -0400

    Adds spawn enemies at random x and at random interval between min/max spawn rate

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject _enemyPrefab;
+    [SerializeField]
+    private float _spawnRateMin = 3.0f;
+    [SerializeField]
+    private float _spawnRateMax = 9.0f;
+    [SerializeField]
+    private bool _spawningAllowed = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (_enemyPrefab == null)
+        {
+            Debug.LogError("SpawnManager::Start() - Enemy Prefab is NULL");
+        }
+
+        StartCoroutine(SpawnEnemies());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator SpawnEnemies()
     {
-        
+        while (_spawningAllowed)
+        {
+            float randomX = Random.Range(-9.0f, 9.0f);
+
+            Vector3 newEnemyPosition = new Vector3(randomX, 7.0f, 0);
+
+            Instantiate(_enemyPrefab, newEnemyPosition, Quaternion.identity);
+
+            yield return new WaitForSeconds(Random.Range(_spawnRateMin, _spawnRateMax));
+        }
     }
 }
