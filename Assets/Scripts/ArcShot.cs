e346727735b61f54afff357a0a9d13bd1023129c
Joseph Youngquist <joseph.youngquist@gmail.com>
2023-05-05 05:09:19 -0400

    finishes the arc shot implementation

--- Assets/Scripts/ArcShot.cs
+++ Assets/Scripts/ArcShot.cs
@@ -10,12 +10,21 @@ public class ArcShot : MonoBehaviour
     [SerializeField]
     private float _arcShotLaserMovementSpeed = 15f;
 
+    void Start()
+    {
+        //float angle = Random.Range(-spreadAngle, spreadAngle);
+        Quaternion rotation = Quaternion.AngleAxis(_arcAngle, Vector3.forward);
+        transform.rotation = rotation;
+
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.up * _arcShotLaserMovementSpeed;
+    }
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.up * _arcShotLaserMovementSpeed * Time.deltaTime);
+        // transform.Translate(Vector3.up * _arcShotLaserMovementSpeed * Time.deltaTime);
 
-        if (transform.position.y > 7.1f)
+        if (transform.position.y > 9.1f)
         {
 
             if (this.transform.parent != null)
