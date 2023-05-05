d3c494e90bac053e7b1844fd48d336327ade2f57
Joseph Youngquist <joseph.youngquist@gmail.com>
2023-05-05 02:57:56 -0400

    adds arc shot prefab and initial behavior script

---
+++ Assets/Scripts/ArcShot.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArcShot : MonoBehaviour
+{
+    [SerializeField]
+    private float _arcAngle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
